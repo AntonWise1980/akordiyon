b803adc2b00dd2a7f3d13f58b03de1c704b2ec68
--replace-all <=>
2025-09-29 22:45:30 +0300

    new image1:   images/1.jpg 	new image2:   images/2.jpg 	new image3:   images/3.jpg 	new image4:   images/4.jpg 	new image5:   images/5.jpg 	new index:   index.html 	new script:   script.js 	new style:   style.css

---
+++ script.js
@@ -0,0 +1,35 @@
+/* Kısaca scriptimin yapmak istediği: önce css class larına ulaşarak
+click eventi ile tetiklenen fonksiyonumla class isimlerini değiştirip
+o elementi aktif olan bir sınıfa geçirmek. Bu işlemi yaparken de
+diğer elementi pasif hale getirmek. HTML sayfasındaki sınıflar
+"panel" ve "panel active" olarak iki tanedir. Bu klass larımızı 
+panel yada panel active olacak şekilde sayfaya js kodumuz ile 
+müdahale etmiş olduk. 
+*/
+
+//panel sınıfı olan tüm elementleri panels objemize aldık.
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel=>{
+//panel itemın her bir item'ına olay dinleyici ekliyorum.
+    panel.addEventListener('click', ()=>{
+// click olayı gerçekleştiğinde yapılacak işlemler başlıyor.
+// çağırdığımız fonksiyon ile paneldeki sınıfların hepsini normal panele çeviriyorum
+// böylelikle hepsi panel class oluyor active olan kalmıyor.
+        removeActiveClasses()
+// tıklama işlemi gerçekleşti, active classlar silindikten sonra
+// şimdide active classı tekrar ekliyoruz. Böylelikle sadece
+// tıklanan panelin sınıfı active hale gelmiş oluyor.       
+        panel.classList.add('active')
+
+// event listener içindeki fonkiyonun kapamaları.
+    })
+// panelsin for each için kapamalar.    
+})
+
+// şimdi yukarıda çağırılan active class ı temizleyecek fonksiyonu yazıyoruz.
+function removeActiveClasses(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+    })
+}
